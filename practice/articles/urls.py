e03aeae3f227c68be4021505766e497c3adb5c63
Hassan Latif <ranashb520@gmail.com>
2021-07-20 16:54:58 +0500

    Named URLS are addeed

--- practice/articles/urls.py
+++ practice/articles/urls.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from django.urls import path,re_path
 from . import views
 
+app_name='articles'
+
 urlpatterns = [
   #  path('admin/', admin.site.urls),
     path(r'',views.article_list),
-    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail,name='details'),
 ]
