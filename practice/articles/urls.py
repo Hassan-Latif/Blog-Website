655f60ec851e17f8053cc6e2474264808b5aedb4
Hassan Latif <ranashb520@gmail.com>
2021-07-20 16:33:16 +0500

    URL patterns are added

--- practice/articles/urls.py
+++ practice/articles/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
   #  path('admin/', admin.site.urls),
     path(r'',views.article_list),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
 ]
--- practice/articles/views.py
+++ practice/articles/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from .models import Article
-
+from django.http import HttpResponse
 # Create your views here.
 def article_list(request):
     articles=Article.objects.all()
 
     return render(request,'articles/article_list.html',{'article':articles})
+
+def article_detail(request,slug):
+    return HttpResponse(slug)
