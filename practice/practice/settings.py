c6c2566b452692ae376c541c3e5c97f8f78655fd
Hassan Latif <ranashb520@gmail.com>
2021-07-24 18:31:09 +0500

    Media files has been added been added

--- practice/practice/settings.py
+++ practice/practice/settings.py
@@ -123,6 +123,9 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=(
     os.path.join(BASE_DIR,'Assets'),
 )
+
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -17,6 +17,9 @@ from django.contrib import admin
 from django.urls import path,include
 from . import view
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
@@ -25,3 +28,4 @@ urlpatterns = [
 ]
 
 urlpatterns += staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
