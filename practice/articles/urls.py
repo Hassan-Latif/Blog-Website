b9a2095c6d9691b6bae742da742148534b21c6fe
Hassan Latif <ranashb520@gmail.com>
2021-07-27 22:23:19 +0500

    Redirecting after login functionality is completed

--- practice/articles/urls.py
+++ practice/articles/urls.py
@@ -7,5 +7,6 @@ app_name='articles'
 urlpatterns = [
   #  path('admin/', admin.site.urls),
     path(r'',views.article_list,name='list'),
+    path('create/',views.article_create,name='create'),
     re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail,name='details'),
 ]
--- practice/articles/views.py
+++ practice/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def article_list(request):
     articles=Article.objects.all()
@@ -12,3 +13,7 @@ def article_detail(request,slug):
     article=Article.objects.get(slug=slug)
     print(article)
     return render(request,'articles/article_details.html',{'articl':article})
+
+@login_required(login_url='accounts:login')
+def article_create(request):
+    return render(request,'articles/article_create.html')
