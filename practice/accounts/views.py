d4ea320fef1cbe704016811e30dd85058f9cb4a4
Hassan Latif <ranashb520@gmail.com>
2021-07-27 19:03:26 +0500

    Login functionality is completed

--- practice/accounts/views.py
+++ practice/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib.auth import login
 # Create your views here.
 
 def signup_view(request):
@@ -8,19 +9,22 @@ def signup_view(request):
         print('its working till here')
         if form.is_valid():
             print('hello world')
-            form.save()
+            user=form.save()
+            login(request,user)
             return redirect('articles:list')
         #Log the user in
     
     else:
         form=UserCreationForm()
-
     return render(request,'accounts/signup.html',{'form':form})
 
 def login_view(request):
     if request.method == 'POST':
         form=AuthenticationForm(data=request.POST)
         if form.is_valid():
+            user=form.get_user()
+            login(request,user)
+
 
             return redirect('articles:list')
 
