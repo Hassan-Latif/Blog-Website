dd543f0b9059fedb0538a00aa3f8ec3a7d16dd7d
Hassan Latif <ranashb520@gmail.com>
2021-08-21 13:10:02 +0500

    Associated an author with articles

--- practice/articles/views.py
+++ practice/articles/views.py
@@ -20,7 +20,9 @@ def article_create(request):
     if request.method=='POST':
         form=forms.CreateArticle(request.POST,request.FILES)
         if form.is_valid():
-            
+            instance=form.save(commit=False)
+            instance.author=request.user
+            instance.save()
             return redirect('articles:list')
     else:
         form=forms.CreateArticle()
