3e1fa7a6f470802f1174f3c1387798a9b9de1f3c
Hassan Latif <ranashb520@gmail.com>
2021-08-21 14:25:12 +0500

    Homepage changed

--- practice/practice/urls.py
+++ practice/practice/urls.py
@@ -19,11 +19,11 @@ from . import view
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
-
+from articles import views as av
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/',include('articles.urls')),
-    path(r'',view.homepage),
+    path(r'',av.article_list,name='home'),
     path(r'about/',view.about),
     path('accounts/',include('accounts.urls')),
 ]
